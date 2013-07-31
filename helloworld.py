898799d323358969c6242fd1a5199623ee9eb60e
OpenShift guest <andrew.beng.2010@sis.smu.edu.sg>
2013-07-31 05:18:25 -0400

    updated helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,15 @@
 import webapp2
+from webapp2_extras import json
 
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('{"hello":"world!"}')
+        self.response.headers['Content-Type'] = 'application/json'
+        hello_world = {
+                'hello': 'world'
+            }
+        self.response.write(json.encode(hello_world))
 
 
 application = webapp2.WSGIApplication([
